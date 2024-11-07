5f22358eb788647a9ee56694cdd8bdaba9e0412e
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2024-11-07 12:42:43 +0700

    fixing: all structure warning successfully fixed

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:toma_scan/ui/pages/widgets/bottom_nav_widget.dart';
+import 'package:toma_scan/ui/widgets/bottom_nav_widget.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
@@ -15,12 +17,12 @@ class _HomePageState extends State<HomePage> {
         title: Row(
           children: [
             Image.asset(
-              'assets/icon.png', // Ganti dengan path ikon 
+              'assets/icon.png', // Ganti dengan path ikon
               width: 30, // Sesuaikan ukuran ikon
               height: 30,
             ),
-            SizedBox(width: 5), // Memberi jarak antara ikon dan teks
-            Text(
+            const SizedBox(width: 5), // Memberi jarak antara ikon dan teks
+            const Text(
               'TomaScan',
               style: TextStyle(
                 fontSize: 20, // Sesuaikan ukuran teks
